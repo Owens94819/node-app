b1c455937e99b6fe95c53fedf8291b4c35640907
Owens94819 <65927654+Owens94819@users.noreply.github.com>
2011-07-10 23:03:33 +0100

    vhost subdomain

--- control room/subdomain.js
+++ control room/subdomain.js
@@ -1,9 +1,9 @@
 module.exports = function(router) {
-    
+
 router.get('/', function(req, res) {
+    console.dir(req.vhost)
     res.send('Welcome to our API!');
 });
-
 
 return router;
 }
--- index.js
+++ index.js
@@ -3,34 +3,32 @@ $__dirname = __dirname;
 require('./control room/global functions')
 
 const express = require('express'),
-    subdomain = require('express-subdomain'),
-router = express.Router(),
-app = express(),
-fs=require('fs'),
-fetch = require('node-fetch'),
-server = app.listen(process.env.PORT || 1234,()=>{
-    var port = server.address().port;
-    console.log(`http://localhost:${port}`);
-});
+    vhost = require('vhost'),
+    router = express.Router(),
+    app = express(),
+    fs = require('fs'),
+    fetch = require('node-fetch'),
+    server = app.listen(process.env.PORT || 1234, () => {
+        var port = server.address().port;
+        console.log(`http://localhost:${port}`);
+    });
 
 app.set('view engine', 'ejs')
 app.use(express.static('./lib/'))
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(require('./control room/header'))
-app.use(subdomain('api', require('./control room/subdomain')(router)));
+app.use(vhost('*.localhost', require('./control room/subdomain')(router)))
 
-fs.readdirSync(path('views/html routers')).forEach(e=>{
+fs.readdirSync(path('views/html routers')).forEach(e => {
     e = parseString(e)[0]
     e = e
-    e= '/'+e
-    $e=e
-    app.get(e,(req,res)=>{
-         e= e.substring(1,e.length)
-        res.render(`html routers/${e}`,{name:e||'home'})
+    e = '/' + e
+    $e = e
+    app.get(e, (req, res) => {
+        e = e.substring(1, e.length)
+        res.render(`html routers/${e}`, { name: e || 'home' })
     })
-
-
 })
 
 
