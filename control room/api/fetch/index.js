973300edd410d232969fcf628d2577a57322a3de
Owens94819 <65927654+Owens94819@users.noreply.github.com>
2011-07-11 00:32:46 +0100

    fetch

--- control room/api/fetch/index.js
+++ control room/api/fetch/index.js
@@ -1,21 +1,36 @@
 module.exports = function (router) {
     router.use(cors({ origin: '*' }))
-    router.use(function(req,res,next){
-         next()
-    })
 
-    router.use(async (req, res)=> {
+    router.use(async (req, res) => {
         var url = req.query.url;
-        var type = req.query.type||'text';
-      
+        var type = req.query.type || 'text',
+            type = type.trim().toLowerCase()
+
+        var method = req.method||'GET'
+
+        //console.log(req.headers);
+
         try {
-            url = await $fetch(url);
-            type = type in url?type:text;
+            url = await $fetch(url,{
+                method,
+                headers:req.headers,
+                //body: req.body
+            });
+
+            type = type in url ? type : text;
+         //   console.log(url.headers);
+
+             var header = url.headers
 
             url = await url[type]();
+            header.forEach((val,key) => {
+                res.header(key,val)
+            });
         } catch (error) {
-            
+              url = 'invalid url definition'
         }
+
+        console.log(res.getHeader('content-type'));
         res.send(url);
     });
 
