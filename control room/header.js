6f9bb63f16c103607fc075068ff11dd041283e50
Owens94819 <65927654+Owens94819@users.noreply.github.com>
2021-08-11 04:19:38 +0100

    Update header.js

--- control room/header.js
+++ control room/header.js
@@ -1,5 +1,15 @@
 module.exports = function (req,res,next) {
   //  res.render('loading')
-  console.log(req.headers);
+  var h = req.headers;
+
+  console.log(`
+  \n\n\n
+  \n--------------------
+  ${h.host}
+  \n--------------------
+  \n--------------------
+  ${h["user-agent"]}
+  \n---------------------
+  `);
   next();
 }
