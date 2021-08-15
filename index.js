0a6086b9f7be72a677f5da6ffe7f04c906cc184e
Owens94819 <65927654+Owens94819@users.noreply.github.com>
2021-08-15 03:07:00 +0100

    node-fetch added

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ $__dirname = __dirname;
 require('./control room/global functions')
 
 const express = require('express'),
-subdomain = require('express-subdomain'),
+    subdomain = require('express-subdomain'),
 router = express.Router(),
 app = express(),
 fs=require('fs'),
+fetch = require('node-fetch'),
 server = app.listen(process.env.PORT || 1234,()=>{
     var port = server.address().port;
     console.log(`http://localhost:${port}`);
@@ -14,6 +15,8 @@ server = app.listen(process.env.PORT || 1234,()=>{
 
 app.set('view engine', 'ejs')
 app.use(express.static('./lib/'))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 app.use(require('./control room/header'))
 app.use(subdomain('api', require('./control room/subdomain')(router)));
 
@@ -21,18 +24,20 @@ fs.readdirSync(path('views/html routers')).forEach(e=>{
     e = parseString(e)[0]
     e = e
     e= '/'+e
+    $e=e
     app.get(e,(req,res)=>{
-     e=req.url;
-     e= e.substring(1,e.length)
+         e= e.substring(1,e.length)
         res.render(`html routers/${e}`,{name:e||'home'})
     })
+
+
 })
 
 
 app.use(require('./control room/404'))
 
 //console.log(fs.readdirSync('/'));
-//heroku logs --tail --app nimo2000
+//heroku logs --tail --app nimo2000x
 
 //git add . 
 //git commit -m 'update'
